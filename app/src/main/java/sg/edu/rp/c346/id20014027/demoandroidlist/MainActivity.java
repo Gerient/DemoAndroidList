b13acb7176285158e3c9443a76c5d877b4748197
20014027 <20014027@rp.edu.sg>
2022-07-24 21:48:27 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20014027/demoandroidlist/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.rp.c346.id20014027.demoandroidlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvAndroidVersions;
+    ArrayList<String> alAndroidVersions;
+    ArrayAdapter<String> aaAndroidVersions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvAndroidVersions = findViewById(R.id.listViewAndroidVersions);
+        alAndroidVersions = new ArrayList<>();
+
+        alAndroidVersions.add("Pie - 9.0");
+        alAndroidVersions.add("Oreo - 8.0 - 8.1");
+        alAndroidVersions.add("Nougat - 7.0 - 7.1.2");
+        alAndroidVersions.add("Marshmallow - 6.0 - 6.0.1");
+        alAndroidVersions.add("Lollipop - 5.0 - 5.1.1");
+
+        aaAndroidVersions = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alAndroidVersions);
+
+        lvAndroidVersions.setAdapter(aaAndroidVersions);
+
+    }
+}
